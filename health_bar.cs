4b563df62784b746d38025bdc3fcb8ada21c6b8b
Yuzuno <129194956+Yuzuno@users.noreply.github.com>
2023-04-14 13:13:39 +0200

    Add files via upload

---
+++ health_bar.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/*NAMING SYNTAX:
+
+constructor xxx of class xxx:       cls_xxx
+struct xxx:                         stc_xxx
+class xxx:                          cls_xxx
+object xxx:                         obj_xxx
+int fct xxx:                        fct_int_xxx
+set int xxx:                        set_int_xxx
+get int xxx:                        get_int_xxx
+int pointer xxx:                    ptr_int_xxx
+
+string var xxx:                     str_xxx
+int var xxx:                        int_xxx
+unsigned int var xxx:               uint_xxx
+const str var xxx:                  STR_xxx
+const int var xxx:                  INT_xxx
+const uint var xxx:                 UINT_xxx
+
+fct int arg xxx:                    int_aXxx
+*/
+
+
+public class health_bar : MonoBehaviour
+{
+    public Slider slider; // Reference to slider component of health bar in unity
+
+    public Gradient gradient;
+
+    public Image fill;
+
+    public void fct_void_set_max_health(int health) // Sets max health value of slider component
+    {
+
+        slider.maxValue=health; // Sets max value of slider component of health bar in unity to health set in unity
+        slider.value=health; // Sets value of slider component of health bar in unity to health set in unity
+
+        fill.color=gradient.Evaluate(1f); // Sets color of health bar to color corresponding to max value of gradient in unity
+
+    }
+
+    public void fct_void_set_health(int health) // Sets health value of slider when damages taken or heals
+    {
+
+        slider.value=health; // Sets value of slider component of health bar in unity to health set in unity
+
+        fill.color=gradient.Evaluate(slider.normalizedValue); // Sets color of health bar to color corresponding to value of gradient in unity (normalized: if value is 20: returns 0.2 because gradient value: 0-1)
+
+    }
+
+}
---
+++ player_health.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+
+/*NAMING SYNTAX:
+
+constructor xxx of class xxx:       cls_xxx
+struct xxx:                         stc_xxx
+class xxx:                          cls_xxx
+object xxx:                         obj_xxx
+int fct xxx:                        fct_int_xxx
+set int xxx:                        set_int_xxx
+get int xxx:                        get_int_xxx
+int pointer xxx:                    ptr_int_xxx
+
+string var xxx:                     str_xxx
+int var xxx:                        int_xxx
+unsigned int var xxx:               uint_xxx
+const str var xxx:                  STR_xxx
+const int var xxx:                  INT_xxx
+const uint var xxx:                 UINT_xxx
+
+fct int arg xxx:                    int_aXxx
+*/
+
+
+
+
+
+public class player_health : MonoBehaviour
+{
+
+    public int max_health=100;
+    public int current_health;
+
+    public health_bar health_bar; // Reference to health bar object in unity
+
+
+    void Start()
+    {
+
+        current_health=max_health; // Sets max value of health to max health set in unity when game started
+        health_bar.fct_void_set_max_health(max_health); // Updates health bar object to max health when game started
+
+    }
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+
+            fct_void_take_damage(5);
+
+        }
+
+    }
+
+    void fct_void_take_damage(int damage)
+    {
+
+        current_health-=damage; // Updates current health after taking damage
+        health_bar.fct_void_set_health(current_health); // Updates health bar object to current health after taking damage
+
+    }
+
+}
